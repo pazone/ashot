5e704d95f7d8a470dc5bf960b22ec15a45f617a2
Alexander Kriegisch <Alexander@Kriegisch.name>
2020-12-12 09:31:44 +0700

    Add explanatory source code comment to WebDriver coordinate calculation

--- src/main/java/pazone/ashot/coordinates/WebDriverCoordsProvider.java
+++ src/main/java/pazone/ashot/coordinates/WebDriverCoordsProvider.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.WebElement;
 /**
  * @author <a href="pazone@yandex-team.ru">Pavel Zorin</a>
  */
+
 public class WebDriverCoordsProvider extends CoordsProvider {
     @Override
     public Coords ofElement(WebDriver driver, WebElement element) {
+        // Keep Point/Dimension as opposed to Rectangle because browsers like PhantomJS do not like the latter
         Point point = element.getLocation();
         Dimension dimension = element.getSize();
         return new Coords(
